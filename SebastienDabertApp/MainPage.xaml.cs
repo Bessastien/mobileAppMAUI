4eec071d8c2a27be6a21bfe81a3fba8c89b2ca1b
Dabert Sébastien <sebdab86@gmail.com>
2026-03-03 19:29:52 +0100

    initProject

---
+++ SebastienDabertApp/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Accessibility;
+
+namespace SebastienDabertApp;
+
+public partial class MainPage : ContentPage
+{
+    private int _count;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object? sender, EventArgs e)
+    {
+        _count++;
+
+        if (_count == 1)
+            CounterBtn.Text = $"Clicked {_count} time";
+        else
+            CounterBtn.Text = $"Clicked {_count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+}
---
+++ SebastienDabertApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SebastienDabertApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
